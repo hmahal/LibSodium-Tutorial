483e81af62d662606419169dc65c512590402586
haro-freezd <harmanmahal@gmail.com>
2018-05-19 18:12:39 -0700

    Updated bank.ts

--- server/bank.ts
+++ server/bank.ts
@@ -1,2 +1,11 @@
-import jsonstream = require('duplex-json-stream');
 import net = require('net');
+
+//TODO: I should try to find typings for this library
+const jsonstream: any = require('duplex-json-stream');
+
+function server(socket: number): net.Server {
+  return net.createServer((socket) => {
+    const jsonStreamSocket = jsonstream(socket);
+    jsonStreamSocket.on('data', (msg: string) => console.log(msg));
+  })
+} 
